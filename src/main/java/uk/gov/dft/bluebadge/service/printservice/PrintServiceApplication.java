0b01e15709e04282cce7da3b0abd38fd4e17aff5
xytrams <yuliya.polyeno@gmail.com>
2018-11-22 12:42:53 +0000

    initial commit

---
+++ src/main/java/uk/gov/dft/bluebadge/service/printservice/PrintServiceApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.dft.bluebadge.service.printservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrintServiceApplication.class, args);
+  }
+}
---
+++ src/test/java/uk/gov/dft/bluebadge/service/printservice/PrintServiceApplicationTests.java
@@ -0,0 +1,31 @@
+package uk.gov.dft.bluebadge.service.printservice;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(
+  classes = PrintServiceApplication.class,
+  properties = {"management.server.port=19991"}
+)
+@RunWith(SpringRunner.class)
+@ActiveProfiles({"test", "dev"})
+public class PrintServiceApplicationTests {
+
+  @Autowired private SecurityConfig securityConfig;
+
+  @Test
+  public void loadContext() {}
+
+  @Test
+  public void shouldInstantiateSecurityConfig() {
+    assertThat(securityConfig, Matchers.notNullValue());
+  }
+}
