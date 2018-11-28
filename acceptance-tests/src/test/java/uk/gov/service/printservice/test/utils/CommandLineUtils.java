a1ffd3a7b8a9bd27b54aa1377c391940fc448f52
Miguel Gil <mgilgar@gmail.com>
2018-11-28 15:29:34 +0000

    [BBB-1047-print-batch-file] Add command line util to verify xml file for print batch request.

---
+++ acceptance-tests/src/test/java/uk/gov/service/printservice/test/utils/CommandLineUtils.java
@@ -0,0 +1,43 @@
+package uk.gov.service.bluebadge.test.utils;
+
+import java.io.IOException;
+import java.lang.ProcessBuilder;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommandLineUtils {
+  private static final Logger log = LoggerFactory.getLogger(CommandLineUtils.class);
+
+  public CommandLineUtils(Map<String, String> config) {
+
+    /*
+    String url = config.get("url");
+    String username = config.get("username");
+    String  ***REMOVED***);
+    String driver = config.get("driverClassName");
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(driver);
+    dataSource.setUrl(url);
+    dataSource.setUsername(username);
+    dataSource.setPassword(password);
+    jdbc = new JdbcTemplate(dataSource);
+    log.info("init jdbc template: {}", url);*/
+  }
+
+  public boolean runScript(String script) throws IOException, InterruptedException {
+    //log.info("Karate Command Line Utils. Running script: {}", script);
+/*
+    //ProcessBuilder pb = new ProcessBuilder("myshellScript.sh", "myArg1", "myArg2");
+    ProcessBuilder pb = new ProcessBuilder(script);
+
+    //    pb.directory(new File("myDir"));
+    Process p = pb.start();
+    p.wait(5000);
+    System.out.println("-------exitValue=" + p.exitValue());
+    System.out.println("-------outputStream=" + p.getOutputStream());
+    System.out.println("-------errorStream=" + p.getErrorStream());
+*/
+    return true;
+  }
+}
